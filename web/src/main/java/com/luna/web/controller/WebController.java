11c27e2a722ee312e383a2ef95b97f20618aacba
luna <iszychen@gmail.com>
2020-03-21 14:17:35 +0800

    init web module

---
+++ web/src/main/java/com/luna/web/controller/WebController.java
@@ -0,0 +1,16 @@
+package com.luna.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Luna@win10
+ * @date 2020/3/21 14:15
+ */
+@Controller
+public class WebController {
+
+	public String web(){
+		return "thymeleaf/demo_3_21";
+	}
+
+}
